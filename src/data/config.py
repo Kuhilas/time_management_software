a3426949e9b9f60dbf130c2b51cadc75d188e543
Tuukka <tuukka.tarkkinen@skillio.ai>
2025-01-28 13:10:14 +0200

    postman done

---
+++ src/data/config.py
@@ -0,0 +1,29 @@
+from configparser import ConfigParser
+import os
+
+def config(filename='src\\data\\database.ini', section='azure'):
+    # Create a parser object to read the configuration
+    parser = ConfigParser()
+    
+    # Check if the .ini file exists
+    if not os.path.exists(filename):
+        raise Exception(f"Configuration file not found at {filename}")
+
+    # Read the configuration file
+    parser.read(filename)
+
+    # Check if the section exists in the .ini file
+    if parser.has_section(section):
+        # Get the parameters of the specified section
+        params = parser[section]
+        db_config = {
+            'host': params['host'],
+            'database': params['database'],
+            'user': params['user'],
+            'password': params['password'],
+            'port': params.get('port', 5432),  # Default to 5432 if not specified
+            'sslmode': params.get('sslmode', 'require')  # Use 'require' if not specified
+         }
+        return db_config
+    else:
+        raise Exception(f"Section {section} not found in the {filename} file")
